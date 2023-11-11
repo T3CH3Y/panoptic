9b68c576539fe6f9ab10f4176c869ef1155ee20f
Ben Zimmerman <business@benzimmerman.org>
2023-11-11 15:38:31 -0500

    caffeine levels at 500mg

--- src/main.py
+++ src/main.py
@@ -1,7 +1,35 @@
 import pinecone
 from options import config
+import numpy as np
+from PIL import Image
 
 pinecone.init(
     api_key = config["pinecone"]["api_key"],
     environment = config["pinecone"]["environment"]
 )
+
+index = pinecone.Index(config["pinecone"]["index"])
+
+test_path = "photos/Hieronymus_Bosch_14.jpg"
+
+def vectorize(img_path):
+
+    im = Image.open(img_path)
+    rgb_data = np.array(im)
+
+    red = rgb_data[:,:,0].flatten()
+    green = rgb_data[:,:,1].flatten()
+    blue = rgb_data[:,:,2].flatten()
+    image_pixels = red.size
+
+    red_histogram, red_bin = np.histogram(red, bins=256, range =(0, 255)) 
+    green_histogram, green_bin = np.histogram(green, bins=256, range =(0, 255)) 
+    blue_histogram, blue_bin = np.histogram(blue, bins=256, range =(0, 255))
+    
+    norm_red = red_histogram/image_pixels
+    norm_green = green_histogram/image_pixels
+    norm_blue = blue_histogram/image_pixels
+
+    img_vec = np.concatenate((norm_red, norm_green, norm_blue)) * 100
+
+    return img_vec
