eb492be61689c51a3215361569f7bc1fef9e1304
Ben Zimmerman <business@benzimmerman.org>
2023-11-11 04:48:29 -0500

    ok I'm going to bed now

--- src/load_data.py
+++ src/load_data.py
@@ -2,8 +2,16 @@ import pinecone
 import os
 from PIL import Image
 import numpy as np
+from options import config
+
+pinecone.init(
+    api_key = config["pinecone"]["api_key"],
+    environment = config["pinecone"]["environment"]
+)
+index = pinecone.Index(config["pinecone"]["image_stuff"])
 
 dir = os.listdir("photos")
+img_vec = np.array([])
 for file_name in dir:
     im = Image.open("photos/" + file_name)
     rgb_data = np.array(im)
@@ -21,5 +29,8 @@ for file_name in dir:
     norm_green = green_histogram/image_pixels
     norm_blue = blue_histogram/image_pixels
 
-print(image_pixels)
+    img_vec = np.concatenate((norm_red, norm_green, norm_blue))
+    print(img_vec.size)
+
+
     
