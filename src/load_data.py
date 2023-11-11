f0883ba1ae948fd134d870be1edc89a38c4a3cd8
Ben Zimmerman <business@benzimmerman.org>
2023-11-11 04:26:28 -0500

    mm gram

--- src/load_data.py
+++ src/load_data.py
@@ -6,5 +6,20 @@ import numpy as np
 dir = os.listdir("photos")
 for file_name in dir:
     im = Image.open("photos/" + file_name)
-    im_matrix = np.array(im)
-    print(im_matrix[0][0])
+    rgb_data = np.array(im)
+
+    red = rgb_data[:,:,0].flatten()
+    green = rgb_data[:,:,1].flatten()
+    blue = rgb_data[:,:,2].flatten()
+    image_pixels = red.size
+
+    red_histogram, red_bin = np.histogram(red, bins=256, range =(0, 255)) 
+    green_histogram, green_bin = np.histogram(green, bins=256, range =(0, 255)) 
+    blue_histogram, blue_bin = np.histogram(blue, bins=256, range =(0, 255))
+    
+    norm_red = red_histogram/image_pixels
+    norm_green = green_histogram/image_pixels
+    norm_blue = blue_histogram/image_pixels
+
+print(image_pixels)
+    
