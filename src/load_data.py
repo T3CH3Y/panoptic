ae889b85c3abf1bcad3e27432cd478bb0468e13c
Ben Zimmerman <business@benzimmerman.org>
2023-11-11 03:05:25 -0500

    read da rgb

--- src/load_data.py
+++ src/load_data.py
@@ -1,4 +1,10 @@
 import pinecone
 import os
+from PIL import Image
+import numpy as np
 
-os.listdir("pictures")
+dir = os.listdir("photos")
+for file_name in dir:
+    im = Image.open("photos/" + file_name)
+    im_matrix = np.array(im)
+    print(im_matrix[0][0])
